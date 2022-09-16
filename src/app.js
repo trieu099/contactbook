1f544fd8e4e95a8aa18484ec92d4a998f04f8706
trieu099 <trieu099@gmail.com>
2022-09-16 22:32:01 +0700

    Project setup

---
+++ src/app.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const ApiError = require('./api-error');
+
+const app = express();
+
+
+app.use(cors());
+app.use(express.json());
+
+const contactController = require('./controllers/contact.controller')
+
+app.get('/', (req, res) => {
+    res.json({message: 'Welcome to contact book application.'});
+});
+
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
+
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
+
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
+
+app.use((req, res, next) => {
+    return next(new ApiError(404, 'Resource not found'));
+});
+
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || 'Internal Server Error',
+    });
+});
+
+
+module.exports = app;
